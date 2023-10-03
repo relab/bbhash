923e7681cf7fd7b92bc8b188ceb8f3115de76171
Hein Meling <hein.meling@uis.no>
2023-10-03 16:46:09 -0700

    Added documentation and commented on NewParallel2 impl

--- bbhash_parallelv2.go
+++ bbhash_parallelv2.go
@@ -31,10 +31,18 @@ func New(gamma float64, partitions int, keys []uint64) (*BBHash2, error) {
 }
 
 // NewParallel2 creates a new BBHash2 for the given keys. The keys must be unique.
+// For small key sets, you may want to use NewSequential instead, since it will likely
+// be faster. NewParallel2 allocates more memory than NewSequential, but will be faster
+// for large key sets.
+//
 // This partitions the input and creates multiple BBHashes using multiple goroutines.
 // The gamma parameter is the expansion factor for the bit vector; the paper recommends
 // a value of 2.0. The larger the value the more memory will be consumed by the BBHash.
 func NewParallel2(gamma float64, numPartitions int, keys []uint64) (*BBHash2, error) {
+	// Partition the keys into numPartitions by placing keys with the
+	// same remainder (modulo numPartitions) into the same partition.
+	// This approach copies the keys into numPartitions slices, which
+	// may lead to some variation in the number of keys in each partition.
 	partitionKeys := make([][]uint64, numPartitions)
 	for _, k := range keys {
 		i := k % uint64(numPartitions)
@@ -60,6 +68,17 @@ func NewParallel2(gamma float64, numPartitions int, keys []uint64) (*BBHash2, er
 	return bb, nil
 }
 
+// Find returns a unique index representing the key in the minimal hash set.
+//
+// The return value is meaningful ONLY for keys in the original key set
+// (provided at the time of construction of the minimal hash set).
+//
+// If the key is in the original key set, the return value is guaranteed to be
+// in the range [1, len(keys)].
+//
+// If the key is not in the original key set, two things can happen:
+// 1. The return value is 0, representing that the key was not in the original key set.
+// 2. The return value is in the expected range [1, len(keys)], but is a false positive.
 func (bb *BBHash2) Find(key uint64) uint64 {
 	i := key % uint64(len(bb.partitions))
 	return bb.partitions[i].Find(key) + uint64(bb.offsets[i])
