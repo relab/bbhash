e7653fa73f9148fd7d2d0b86d7823a965ba02503
Hein Meling <hein.meling@uis.no>
2025-02-10 22:48:48 +0100

    refactor(bitVector): adds AppendBinary for the bitVector type
    This adds the new AppendBinary method for the bitVector type along
    with tests and benchmarks for BinaryMarshal and BinaryUnmarshal
    methods.

--- bitvector_marshal.go
+++ bitvector_marshal.go
@@ -6,55 +6,53 @@ import (
 	"fmt"
 )
 
+const uint64bytes = 8
+
 // marshaledLength returns the number of bytes needed to marshal the bit vector.
 func (b bitVector) marshaledLength() int {
-	const uint64bytes = 8
 	return uint64bytes * (1 + len(b))
+}
+
+// AppendBinary implements the [encoding.BinaryAppender] interface.
+func (b bitVector) AppendBinary(buf []byte) ([]byte, error) {
+	// append the number of words needed for this bit vector to the buffer
+	buf = binary.LittleEndian.AppendUint64(buf, b.words())
+	// append the bit vector entries to the buffer
+	for _, v := range b {
+		buf = binary.LittleEndian.AppendUint64(buf, v)
+	}
+	return buf, nil
 }
 
 // MarshalBinary implements the [encoding.BinaryMarshaler] interface.
 func (b bitVector) MarshalBinary() ([]byte, error) {
-	out := make([]byte, b.marshaledLength())
-	// Make a copy of out, since we will be modifying buf's slice indices
-	buf := out
-
-	// Write the number of uint64 words needed for this bit vector to the out buffer
-	binary.LittleEndian.PutUint64(buf[:8], b.words())
-	buf = buf[8:]
-
-	// Write the bit vector entries to the out buffer
-	for _, v := range b {
-		binary.LittleEndian.PutUint64(buf[:8], v)
-		buf = buf[8:]
-	}
-
-	return out, nil
+	return b.AppendBinary(make([]byte, 0, b.marshaledLength()))
 }
 
 // UnmarshalBinary implements the [encoding.BinaryUnmarshaler] interface.
 func (b *bitVector) UnmarshalBinary(data []byte) error {
 	// Make a copy of data, since we will be modifying buf's slice indices
 	buf := data
-	if len(buf) < 8 {
+	if len(buf) < uint64bytes {
 		return errors.New("bitVector.UnmarshalBinary: no data")
 	}
 
 	// Read the number of words in the bit vector
-	words := binary.LittleEndian.Uint64(buf[:8])
+	words := binary.LittleEndian.Uint64(buf[:uint64bytes])
 	if words == 0 || words > (1<<32) {
 		return fmt.Errorf("bitVector.UnmarshalBinary: invalid length %d", words)
 	}
 
 	*b = make(bitVector, words) // modify b in place
-	buf = buf[8:]
+	buf = buf[uint64bytes:]
 
 	// Read the bit vector entries
 	for i := uint64(0); i < words; i++ {
-		if len(buf) < 8 {
+		if len(buf) < uint64bytes {
 			return errors.New("bitVector.UnmarshalBinary: not enough data to read bit vector entry")
 		}
-		(*b)[i] = binary.LittleEndian.Uint64(buf[:8])
-		buf = buf[8:]
+		(*b)[i] = binary.LittleEndian.Uint64(buf[:uint64bytes])
+		buf = buf[uint64bytes:]
 	}
 
 	return nil
--- bitvector_test.go
+++ bitvector_test.go
@@ -1,6 +1,7 @@
 package bbhash
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -46,4 +47,83 @@ func TestSetIsSet(t *testing.T) {
 			}
 		}
 	}
+}
+
+func TestBitVectorMarshalUnmarshalBinary(t *testing.T) {
+	for _, words := range []uint64{1, 10, 100, 1000, 10000, 100000} {
+		t.Run(fmt.Sprintf("words=%d", words), func(t *testing.T) {
+			mbv := fillBitVector(words)
+			data, err := mbv.MarshalBinary()
+			if err != nil {
+				t.Fatalf("MarshalBinary() failed: %v", err)
+			}
+
+			var ubv bitVector
+			if err := ubv.UnmarshalBinary(data); err != nil {
+				t.Fatalf("UnmarshalBinary() failed: %v", err)
+			}
+
+			if !mbv.equal(ubv) {
+				t.Errorf("bit vectors do not match")
+			}
+		})
+	}
+}
+
+func TestBitVectorAppendBinary(t *testing.T) {
+	for _, words := range []uint64{1, 10, 100, 1000, 10000, 100000} {
+		t.Run(fmt.Sprintf("words=%d", words), func(t *testing.T) {
+			abv := fillBitVector(words)
+			data, err := abv.AppendBinary(nil)
+			if err != nil {
+				t.Fatalf("AppendBinary() failed: %v", err)
+			}
+
+			var ubv bitVector
+			if err := ubv.UnmarshalBinary(data); err != nil {
+				t.Fatalf("UnmarshalBinary() failed: %v", err)
+			}
+
+			if !abv.equal(ubv) {
+				t.Errorf("bit vectors do not match")
+			}
+		})
+	}
+}
+
+func BenchmarkBitVectorMarshalBinary(b *testing.B) {
+	for _, words := range []uint64{1, 10, 100, 1000, 10000, 100000} {
+		bv := fillBitVector(words)
+		b.Run(fmt.Sprintf("words=%d", words), func(b *testing.B) {
+			for b.Loop() {
+				_, _ = bv.MarshalBinary()
+			}
+		})
+	}
+}
+
+func BenchmarkBitVectorUnmarshalBinary(b *testing.B) {
+	for _, words := range []uint64{1, 10, 100, 1000, 10000, 100000} {
+		bv := fillBitVector(words)
+		data, err := bv.MarshalBinary()
+		if err != nil {
+			b.Fatalf("MarshalBinary() failed: %v", err)
+		}
+		b.Run(fmt.Sprintf("words=%d", words), func(b *testing.B) {
+			var bv bitVector
+			for b.Loop() {
+				_ = bv.UnmarshalBinary(data)
+			}
+		})
+	}
+}
+
+func fillBitVector(words uint64) bitVector {
+	bv := make(bitVector, words)
+	for i := uint64(0); i < bv.size(); i++ {
+		if i%2 == 0 {
+			bv.set(i)
+		}
+	}
+	return bv
 }
