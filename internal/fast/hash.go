416ffc351f6aed434a9eaf17f7819b4af3744332
Hein Meling <hein.meling@uis.no>
2025-02-14 22:40:25 +0100

    chore: remove unused rand64 function and related imports

--- internal/fast/hash.go
+++ internal/fast/hash.go
@@ -1,10 +1,5 @@
 package fast
 
-import (
-	"crypto/rand"
-	"encoding/binary"
-)
-
 const m uint64 = 0x880355f21e6d1965
 
 // Hash returns the hash of the current level and key.
@@ -32,10 +27,4 @@ func mix(h uint64) uint64 {
 	h *= 0x2127599bf4325c37
 	h ^= h >> 47
 	return h
-}
-
-// rand64 returns a 64-bit cryptographic random number.
-func rand64() (rnd uint64) {
-	binary.Read(rand.Reader, binary.LittleEndian, &rnd)
-	return
 }
