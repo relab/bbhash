7598e52ee80b8941a042d43561b65a84fc035772
Creeger <magnus.tysdal@gmail.com>
2025-02-17 22:09:26 +0100

    Resolved the PR change requests regarding Chunks(), Keys() and their tests

--- bbhash_iter.go
+++ bbhash_iter.go
@@ -1,57 +1,33 @@
 package bbhash
 
 import (
-	"crypto/sha256"
-	"encoding/binary"
 	"io"
 	"iter"
-
-	"github.com/relab/bbhash/internal/fast"
 )
 
-type BBHashSeq struct {
-	bits       []*bitVector
-	ranks      []uint64
-	reverseMap []uint64
-	hashFunc   int // Use int instead of bool incase there can be more hash functions tested than fasthash64 and SHA256. 0 = hash64, 1 = SHA256.
-	MaxLevel   int
-}
-
 // Find the chunks from slow memory
-// Currently only takes the whole sequence of input and crashes if it's not able to fit everything in the buffer.
+// Chunks returns smaller chunks of data given a reader with some data already being read and with the buffer size.
 func ReadChunks(readerInfo io.Reader, bufSz int) iter.Seq[[]byte] {
 	return func(yield func([]byte) bool) {
-		i := 0
+		buffer := make([]byte, bufSz)
 		for {
 			// Create buffer and read the input into it for a certain range of bytes
-			buffer := make([]byte, bufSz)
-			_, err := readerInfo.Read(buffer)
+			n, err := readerInfo.Read(buffer)
 			if err != nil {
 				return
 			}
-			i++
-			if !yield(buffer) {
+			if !yield(buffer[:n]) {
 				return
 			}
 		}
 	}
 }
 
-// Int = hashfunc, iter.Seq chunks.
-// Recieves chunks from chunks() and converts them to keys and returns them as []uint64
-func Keys(hashFunc int, chunks iter.Seq[[]byte]) []uint64 {
+// Keys returns the hashes of the chunks using the provided hash function
+func Keys(hashFunc func([]byte) uint64, chunks iter.Seq[[]byte]) []uint64 {
 	var keys []uint64
-	if hashFunc == 0 {
-		for c := range chunks {
-			keys = append(keys, fast.Hash64(1, c)) //1 is just a magic number
-		}
-	} else {
-		for c := range chunks {
-			h := sha256.New()
-			h.Write(c)
-			s := binary.LittleEndian.Uint64(h.Sum(nil))
-			keys = append(keys, s)
-		}
+	for c := range chunks {
+		keys = append(keys, hashFunc(c))
 	}
 
 	return keys
